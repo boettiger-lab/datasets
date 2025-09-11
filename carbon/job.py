ac248cc02cfa429c5831a25d0c889f137bbafb1d
Carl Boettiger <cboettig@berkeley.edu>
2025-09-11 10:37:58 -0700

    try 2

--- carbon/job.py
+++ carbon/job.py
@@ -24,20 +24,28 @@ df = (con
 zoom = 8
 
 
-# smoke-test
-i = 100
-wkt = df.geom[i]
-h0 = df.h0[i]
-gdal.Warp("/tmp/carbon.xyz", input_url, dstSRS = 'EPSG:4326', cutlineWKT = wkt, cropToCutline = True)
-(con
-  .read_csv("/tmp/carbon.xyz", 
-            delim = ' ', 
-            columns = {'X': 'FLOAT', 'Y': 'FLOAT', 'Z': 'INTEGER'})
-  .mutate(h0 = h3_latlng_to_cell_string(_.Y, _.X, zoom),
-          h8 = h3_latlng_to_cell_string(_.Y, _.X, zoom))
-  .mutate(Z = ibis.ifelse(_.Z == 65535, None, _.Z)) 
- .to_parquet("/tmp/test.parquet")
-  #.to_parquet(f"s3://public-carbon/hex/vulnerable-carbon/h0={h0}/vulnerable-total-carbon-2018-h{zoom}.parquet")
-)
-pathlib.Path("/tmp/carbon.xyz").unlink()
-con.read_parquet("/tmp/test.parquet")
+
+for i in range(df.shape[0]):
+    wkt = df.geom[i]
+    h0 = df.h0[i]
+    zoom = 8
+    print(h0)
+
+    try:
+        gdal.Warp("/tmp/carbon.xyz", input_url, dstSRS = 'EPSG:4326', cutlineWKT = wkt, cropToCutline = True)
+    
+        (con
+          .read_csv("/tmp/carbon.xyz", 
+                    delim = ' ', 
+                    columns = {'X': 'FLOAT', 'Y': 'FLOAT', 'Z': 'INTEGER'})
+          .mutate(h0 = h3_latlng_to_cell_string(_.Y, _.X, zoom),
+                  h8 = h3_latlng_to_cell_string(_.Y, _.X, zoom))
+          .mutate(Z = ibis.ifelse(_.Z == 65535, None, _.Z)) 
+          .to_parquet(f"s3://public-carbon/hex/vulnerable-carbon/h0={h0}/vulnerable-total-carbon-2018-h{zoom}.parquet")
+        )
+        pathlib.Path("/tmp/carbon.xyz").unlink()
+    except Exception as e:
+        print(f"Error processing item {i}: {e}")
+    
+
+
