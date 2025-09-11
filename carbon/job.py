8595a6df4c8abb9e20d2f4d678ab3603bb24837f
Carl Boettiger <cboettig@berkeley.edu>
2025-09-11 10:19:34 -0700

    h3 debug

--- carbon/job.py
+++ carbon/job.py
@@ -8,8 +8,8 @@ from cng.h3 import *
 from ibis import _
 
 gdal.DontUseExceptions()
-con = ibis.duckdb.connect("/tmp/duck.db",extensions = ["spatial", "h3"])
 install_h3()
+con = ibis.duckdb.connect("/tmp/duck.db",extensions = ["spatial", "h3"])
 set_secrets(con)
 
 input_url = "/vsicurl/https://minio.carlboettiger.info/public-carbon/cogs/vulnerable_c_total_2018.tif"
@@ -18,27 +18,20 @@ df = con.read_parquet("s3://public-grids/hex/h0.parquet").mutate(h0 = _.h0.lower
 zoom = 8
 
 
-
-for i in range(df.shape[0]):
-    wkt = df.geom[i]
-    h0 = df.h0[i]
-    zoom = 8
-    print(h0)
-
-    try:
-        gdal.Warp("/tmp/carbon.xyz", input_url, dstSRS = 'EPSG:4326', cutlineWKT = wkt, cropToCutline = True)
-    
-        (con
-          .read_csv("/tmp/carbon.xyz", 
-                    delim = ' ', 
-                    columns = {'X': 'FLOAT', 'Y': 'FLOAT', 'Z': 'INTEGER'})
-          .mutate(h0 = h3_latlng_to_cell_string(_.Y, _.X, zoom),
-                  h8 = h3_latlng_to_cell_string(_.Y, _.X, zoom))
-          .mutate(Z = ibis.ifelse(_.Z == 65535, None, _.Z)) 
-          .to_parquet(f"s3://public-carbon/hex/vulnerable-carbon/h0={h0}/vulnerable-total-carbon-2018-h{zoom}.parquet")
-        )
-        pathlib.Path("/tmp/carbon.xyz").unlink()
-    except Exception as e:
-        print(f"Error processing item {i}: {e}")
-    
-
+# smoke-test
+i = 100
+wkt = df.geom[i]
+h0 = df.h0[i]
+gdal.Warp("/tmp/carbon.xyz", input_url, dstSRS = 'EPSG:4326', cutlineWKT = wkt, cropToCutline = True)
+(con
+  .read_csv("/tmp/carbon.xyz", 
+            delim = ' ', 
+            columns = {'X': 'FLOAT', 'Y': 'FLOAT', 'Z': 'INTEGER'})
+  .mutate(h0 = h3_latlng_to_cell_string(_.Y, _.X, zoom),
+          h8 = h3_latlng_to_cell_string(_.Y, _.X, zoom))
+  .mutate(Z = ibis.ifelse(_.Z == 65535, None, _.Z)) 
+ .to_parquet("/tmp/test.parquet")
+  #.to_parquet(f"s3://public-carbon/hex/vulnerable-carbon/h0={h0}/vulnerable-total-carbon-2018-h{zoom}.parquet")
+)
+pathlib.Path("/tmp/carbon.xyz").unlink()
+con.read_parquet("/tmp/test.parquet")
