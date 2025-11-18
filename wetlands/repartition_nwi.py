629c134e3fdb80d7e28392fd7de0d982c94e3926
Carl Boettiger <cboettig@berkeley.edu>
2025-11-17 16:14:30 -0800

    attempt single job for repartition

---
+++ wetlands/repartition_nwi.py
@@ -0,0 +1,14 @@
+
+import ibis
+from ibis import _
+from cng.utils import *
+con = ibis.duckdb.connect("local.db")
+set_secrets(con)
+
+print("Re-paritioning....")
+(con
+ .read_parquet("s3://public-wetlands/nwi/hexchunks/**")
+ .to_parquet("s3://public-wetlands/nwi/hex/", partition_by="h0")
+)
+print("Done!")
+
