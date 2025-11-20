b240eea74b46569c3443f65a2b6ec8e788461079
Carl Boettiger <cboettig@gmail.com>
2025-11-20 02:05:12 +0000

    update

--- wetlands/repartition_nwi.py
+++ wetlands/repartition_nwi.py
@@ -2,15 +2,20 @@
 import ibis
 from ibis import _
 from cng.utils import *
-con = ibis.duckdb.connect("/tmp/duck.db")
+from cng.h3 import * 
+con = ibis.duckdb.connect("/tmp/duck.db", extensions = ["spatial", "h3"])
+install_h3()
+
 set_secrets(con)
 
 #con.raw_sql("SET threads TO 100")
+con.raw_sql("SET preserve_insertion_order=false") # saves RAM
 
 print("Re-paritioning....")
 (con
- .read_parquet("s3://public-wetlands/nwi/chunks/**")
- .to_parquet("s3://public-wetlands/nwi/hex/", partition_by="h0")
+ .read_parquet("s3://public-wetlands/nwi/chunks/**").
+ .mutate(h1 = h3_cell_to_parent(_.h8, 1))
+ .to_parquet("s3://public-wetlands/nwi/hex/", partition_by="h1")
 )
 print("Done!")
 
