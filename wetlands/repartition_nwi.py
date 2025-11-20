8b470ec89cb811953da1e5de6126a29b0a1ec4da
Carl Boettiger <cboettig@berkeley.edu>
2025-11-19 21:49:20 -0800

    typo

--- wetlands/repartition_nwi.py
+++ wetlands/repartition_nwi.py
@@ -11,6 +11,5 @@ set_secrets(con)
 #con.raw_sql("SET threads TO 100")
 con.raw_sql("SET preserve_insertion_order=false") # saves RAM
 
-%%time
 con.raw_sql("create table wetlands from scan_parquet('s3://public-wetlands/nwi/chunks/**')")
 con.raw_sql("copy wetlands to s3://public-wetlands/nwi/nwi_h8.parquet")
