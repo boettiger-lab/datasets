65765dc07b193e942740bca0d0866713c046202b
Carl Boettiger <cboettig@berkeley.edu>
2025-11-20 07:54:49 -0800

    type

--- wetlands/repartition_nwi.py
+++ wetlands/repartition_nwi.py
@@ -15,6 +15,13 @@ con.raw_sql("SET preserve_insertion_order=false") # saves RAM
 #con.raw_sql("copy wetlands to 's3://public-wetlands/nwi/hex' (FORMAT PARQUET, PARTITION_BY (h1))")
 
 
+
+@ibis.udf.scalar.builtin
+def h3_cell_to_parent(cell, zoom: int) -> str:
+    ...
+
+
+
 chunks = con.read_parquet("s3://public-wetlands/nwi/chunks/**")
 chunks_h1 = chunks.mutate(h1 = h3_cell_to_parent(_.h8, 1))
 h1 = chunks_h1.select("h1").distinct().execute()["h1"].tolist()
