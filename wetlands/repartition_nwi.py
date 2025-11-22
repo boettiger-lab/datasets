61050b1e28fda148c794fd6e70064db0274476cc
Carl Boettiger <cboettig@berkeley.edu>
2025-11-22 14:23:53 -0800

    Modify repartition_nwi.py to write to /tmp/hex before uploading to S3

--- wetlands/repartition_nwi.py
+++ wetlands/repartition_nwi.py
@@ -1,7 +1,9 @@
 import ibis
 from ibis import _
 from cng.utils import *
- 
+import os
+import shutil
+
 con = ibis.duckdb.connect()
 set_secrets(con)
 con.raw_sql("SET preserve_insertion_order=false") # saves RAM
@@ -9,10 +11,19 @@ con.raw_sql("SET http_timeout=1200")
 con.raw_sql("SET http_retries=30")
 con.raw_sql("SET s3_socket_timeout=1200")
 
+# Create /tmp/hex directory if it doesn't exist
+local_dir = "/tmp/hex"
+os.makedirs(local_dir, exist_ok=True)
 
-# remove NA
+# remove NA and write to local directory first
 (con
     .read_parquet("s3://public-wetlands/nwi/bigchunks/**").
-    .to_parquet("s3://public-wetlands/nwi/hex/", partition_by = "h0")
+    to_parquet(f"{local_dir}/", partition_by = "h0")
 )
+
+# Upload from local directory to S3
+con.raw_sql(f"COPY FROM '{local_dir}/*' TO 's3://public-wetlands/nwi/hex/' (FORMAT PARQUET)")
+
+# Clean up local directory
+shutil.rmtree(local_dir)
 
