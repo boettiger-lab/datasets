6f1b0d0c179060eafb68a110f5e6eff0a7f0b372
Carl Boettiger <cboettig@berkeley.edu>
2025-11-22 08:28:32 -0800

    repartition?

--- wetlands/repartition_nwi.py
+++ wetlands/repartition_nwi.py
@@ -3,12 +3,16 @@ from ibis import _
 from cng.utils import *
  
 con = ibis.duckdb.connect()
-set_secrets(con, "", "", "s3-west.nrp-nautilus.io")
+set_secrets(con)
 con.raw_sql("SET preserve_insertion_order=false") # saves RAM
+con.raw_sql("SET http_timeout=1200")
+con.raw_sql("SET http_retries=30")
+con.raw_sql("SET s3_socket_timeout=1200")
 
 
+# remove NA
 (con
-    .read_parquet("s3://public-wetlands/nwi/bigchunks/**")
+    .read_parquet("s3://public-wetlands/nwi/bigchunks/**").
     .to_parquet("s3://public-wetlands/nwi/hex/", partition_by = "h0")
 )
 
