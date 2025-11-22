c06bf26fcec9286e70c7a6fb6963e95e4cf97b19
Carl Boettiger <cboettig@berkeley.edu>
2025-11-22 14:24:56 -0800

    Fix: remove unsupported s3_socket_timeout parameter

--- wetlands/repartition_nwi.py
+++ wetlands/repartition_nwi.py
@@ -9,7 +9,6 @@ set_secrets(con)
 con.raw_sql("SET preserve_insertion_order=false") # saves RAM
 con.raw_sql("SET http_timeout=1200")
 con.raw_sql("SET http_retries=30")
-con.raw_sql("SET s3_socket_timeout=1200")
 
 # Create /tmp/hex directory if it doesn't exist
 local_dir = "/tmp/hex"
