5819d25a3c5c154237e6934fb39468bb84634013
Carl Boettiger <cboettig@berkeley.edu>
2025-12-06 12:55:53 -0800

    Fix UTF-8 encoding for Ramsar parquet creation

--- wetlands/ramsar/create_parquet.sh
+++ wetlands/ramsar/create_parquet.sh
@@ -6,10 +6,13 @@ set -e
 echo "Converting Ramsar shapefile to GeoParquet..."
 
 # Convert shapefile to GeoParquet
+# Force recoding to UTF-8 to handle special characters
 ogr2ogr \
   -f Parquet \
   /vsis3/public-wetlands/ramsar/ramsar_wetlands.parquet \
   /vsicurl/https://minio.carlboettiger.info/public-wetlands/ramsar/features_publishedPolygon.shp \
+  -lco ENCODING=UTF-8 \
+  -oo ENCODING=UTF-8 \
   -progress
 
 echo "GeoParquet created successfully!"
