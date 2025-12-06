3ffbdecd4d2627064f018a24e759dbabe8d27d49
Carl Boettiger <cboettig@berkeley.edu>
2025-12-06 13:11:11 -0800

    Write parquet locally then upload with boto3 to S3

--- wetlands/ramsar/create_parquet.sh
+++ wetlands/ramsar/create_parquet.sh
@@ -9,10 +9,12 @@ echo "Converting Ramsar shapefile to GeoParquet..."
 python3 << 'EOPYTHON'
 import geopandas as gpd
 import warnings
+import boto3
+import os
+
 warnings.filterwarnings('ignore')
 
 # Read shapefile with Latin-1 encoding (common for shapefiles with special chars)
-# Then write to parquet which will be UTF-8
 gdf = gpd.read_file(
     'https://minio.carlboettiger.info/public-wetlands/ramsar/features_publishedPolygon.shp',
     encoding='latin1'
@@ -23,11 +25,25 @@ for col in gdf.select_dtypes(include=['object']):
     if col != 'geometry':
         gdf[col] = gdf[col].apply(lambda x: x.encode('utf-8', errors='replace').decode('utf-8') if isinstance(x, str) else x)
 
-# Write to S3 as parquet
-gdf.to_parquet(
-    's3://public-wetlands/ramsar/ramsar_wetlands.parquet',
-    compression='snappy'
+# Write to local temp file
+local_path = '/tmp/ramsar_wetlands.parquet'
+gdf.to_parquet(local_path, compression='snappy')
+
+print("Local parquet created, uploading to S3...")
+
+# Upload to S3
+s3_client = boto3.client(
+    's3',
+    endpoint_url=f"http://{os.environ['AWS_S3_ENDPOINT']}",
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
 )
 
-print("GeoParquet created successfully!")
+s3_client.upload_file(
+    local_path,
+    'public-wetlands',
+    'ramsar/ramsar_wetlands.parquet'
+)
+
+print("GeoParquet created and uploaded successfully!")
 EOPYTHON
